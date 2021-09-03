709d8fe8afd6771129cf5e7f26bf47b24af527fb
Tiago <tiagosilvapereira3@gmail.com>
2021-09-03 16:59:08 -0300

    Initial commit

---
+++ src/Component.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="w-full">
+        Admin LTE plugin
+        <!-- <label class="block text-sm font-bold">Tenant Field Name</label>
+        <small class="mb-2">The field that identifies the relationship of a Model with the Tenant (authenticated user)</small>
+        <input class="input" type="text" v-model="tenantFieldName" @input="save"> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pluginData: {},
+            sidebarMode: 'dark',
+        }
+    },
+
+    created() {
+        this.pluginData = window.vemtoApi.getPluginData()
+        this.sidebarMode = this.pluginData.sidebarMode
+    },
+
+    methods: {
+        save() {
+            window.vemtoApi.savePluginData({
+                sidebarMode: this.sidebarMode,
+            })
+        }
+    }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,37 @@
+module.exports = (vemto) => {
+
+    return {
+
+        canInstall() {
+            let projectSettings = vemto.getProjectSettings()
+
+            if(projectSettings.cssFramework !== 'bootstrap') {
+                vemto.addBlockReason('This plugin is only compatible with Bootstrap projects')
+                return false
+            }
+
+            if(projectSettings.uiTemplate !== 'laravel_ui') {
+                vemto.addBlockReason('This plugin is only compatible with Laravel UI templates')
+                return false
+            }
+            
+            return true
+        },
+
+        onInstall() {
+            vemto.savePluginData({
+                sidebarMode: 'dark'
+            })
+        },
+
+        beforeCodeGenerationStart() {
+            vemto.log.message('Replacing stubs for Admin LTE...')
+            
+            let basePath = '/views/frameworks/bootstrap'
+
+            vemto.replaceStub(`${basePath}/AppLayout.vemtl`, 'files/AppLayout.vemtl')
+        }
+
+    }
+
+}
